c434f7e85a9f40331d28e60b63f75d86a9e03ee2
shailesh yadav <shaileshyadv1993@gmail.com>
2024-07-09 21:59:46 +0545

    Connect to mongoDB

---
+++ src/index.js
@@ -0,0 +1,33 @@
+// require("dotenv").config({ path: "./env" });
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import express from "express";
+
+const app = express();
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+
+// app.listen(process.env.POR, () => {
+//   console.log("Server is up and running");
+// });
+/*
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("ERROR:", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("ERROR:", error);
+    throw err;
+  }
+})();
+*/
